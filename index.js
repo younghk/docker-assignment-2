047a2e8574cb032f47389d06f966d68c81e3d640
cheong <hwa1130k@gmail.com>
2018-12-19 01:53:00 +0900

    .16

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const promisify = require('util').promisify;
-const os = require('os');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const outdent = require('outdent');
 const lockfile = require('proper-lockfile');
 
 const PORT = 8080;
@@ -13,16 +11,9 @@ const HOST = '0.0.0.0';
 const CACHE_PATH =  process.argv[2] || '/data';
 const LAST_CACHE_FILENAME = '.last';
 
-const LOG_CACHE_FILENAME = '.loglast';
-
 function touchSync(path){
     if (fs.existsSync(path)) return;
     fs.closeSync(fs.openSync(path, 'w'));
-}
-
-function touchSync2(path){
-    if (fs.existsSync(path)) return;
-    fs.closeSync(fs.openSync(path, 'a'));
 }
 
 ((logPath)=>{
@@ -60,51 +51,13 @@ const writeLog = ((cacheTo) => {
     path.join(CACHE_PATH, LAST_CACHE_FILENAME)
 );
 
-const writeLog2 = ((cacheTo) => {
-    touchSync2(cacheTo);
-    console.log("cacheTo is : ",cacheTo)
-    const rf = promisify(fs.readFile);
-    const wf = promisify(fs.writeFile);
-
-    function readLast2() { return rf(cacheTo); }
-    function writeLast2(msg){ return wf(cacheTo, msg, {
-        flag: 'a'
-    }) }
-
-    return (msg) => {
-        let last = null;
-        let release = null;
-
-        return lockfile.lock(cacheTo).then((_release) => {
-            release = _release;
-            return readLast2();
-        }).then((lastMsg)=>{
-            last = lastMsg;
-            console.log("msg is :: ",msg);
-            return writeLast2(msg+'\n');
-        }).then(()=>{
-            release();
-            return last;
-        })
-    }
-})( path.join(CACHE_PATH, LOG_CACHE_FILENAME) );
-
 const app = express();
-app.get('/', (req, res) => {
-    res.send(
-        outdent`Hello Container!
-                Total memory size of container: ${(os.totalmem() / 1024 / 1024)} MiB` + '\n');
-}).get('/:whatever', (req, res) => {
+app.get('/:whatever', (req, res) => {
     const msg = `${Date.now()},${req.params.whatever}`;
     console.log(msg);
     writeLog(msg).then((last)=>{
         res.send('' + last + '\n' + msg + '\n');
-        writeLog2(msg).then((last)=>{
-            console.log('doing...')
-//            res.send(''+last+'\n'+msg+' :: has been added into loglast\n');
-        })
     })
-
 });
 
 app.listen(PORT, HOST);
